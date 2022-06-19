89200f1e75d0a5e36f54cd0e0d9b0c9608b1adc4
Pablo Mac <pcassettai@gmail.com>
2022-06-19 19:00:19 -0300

    Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const { response } = require('express');
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
+const { json } = require('express/lib/response');
 
 const login = async(req, res = response)=>{
   const {correo, password} = req.body;
@@ -43,6 +45,55 @@ const login = async(req, res = response)=>{
   }
 }
 
+const googleSignIn = async(req, res = response)=>{
+  const { id_token } = req.body;
+
+  try {
+    const { correo, nombre, img } = await googleVerify(id_token);
+    
+    let usuario = await Usuario.findOne({correo});
+
+    if( !usuario ){
+      // crea el usuario, al ser de google, los campos que no trae le pongo basura
+      const data = {
+        nombre,
+        correo,
+        rol:'USER_ROLE',
+        password:':P',
+        img,
+        google: true
+      }
+
+      usuario = new Usuario( data );
+      await usuario.save();
+    }
+
+    // Verifica estado del usuario en DB
+    if( !usuario.estado ){
+      return res.status(401).json({
+        msg:'Hable con el administrador, usuario bloqueado',
+      })
+    }
+
+    // Genera JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token
+    });
+    
+  } catch (error) {
+    res.status(400).json({
+      ok:false,
+      msg:'El token no se pudo verificar',
+      error
+    })
+  }
+
+}
+
 module.exports = {
-  login
+  login,
+  googleSignIn
 }
